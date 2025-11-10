eb50e0d50039f6319b2373513e2edd9be00cae36
afifudin23 <afifu5882@gmail.com>
2025-11-10 21:50:05 +0700

    feat : add model, resource, and feature books

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
+use App\Http\Controllers\BookController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::controller(BookController::class)->group(function () {
+    Route::get('/books', 'index')->name('books.index');
+    Route::post('/books', 'store')->name('books.store');
+    Route::get('/books/{id}/edit', 'edit')->name('books.edit');
+    Route::put('/books/{id}', 'update')->name('books.update');
+    Route::delete('/books/{id}', 'destroy')->name('books.destroy');
 });
