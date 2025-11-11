280f17fb01d609eda831147c98ccfcddd345991e
afifudin23 <afifu5882@gmail.com>
2025-11-11 09:41:53 +0700

    feat: deploy project to vercel app

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         
-        <link rel="icon" type="image/png" href="{{ asset('elibrary.png') }}">
+        <link rel="icon shortcut" type="image/png" href="{{ asset('elibrary.png') }}">
         <title>{{ config('app.name', 'Laravel') }}</title>
 
         <!-- Fonts -->
