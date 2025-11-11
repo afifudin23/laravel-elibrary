3119ee37d9e90298d2609c00bf9ba26e190378d0
afifudin23 <afifu5882@gmail.com>
2025-11-11 09:11:39 +0700

    feat: deploy project to vercel app

--- resources/views/books/index.blade.php
+++ resources/views/books/index.blade.php
@@ -6,6 +6,7 @@
     <title>E-Library</title>
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="icon" type="image/png" href="{{ asset('favicon.png') }}">
     <!-- Font Awesome untuk ikon -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <style>
